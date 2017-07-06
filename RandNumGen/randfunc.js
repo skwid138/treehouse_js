86255fb4cd6505371c3891e0eb6e7e59f44c585c
skwid138 <skwid138@gmail.com>
2017-07-06 12:08:27 -0500

    created a new random number generator using functions, new html file for it, debugged and commented

---
+++ RandNumGen/randfunc.js
@@ -0,0 +1,33 @@
+/*
+This app should ask the user for 2 numbers
+determine which is lower and which is higher
+calculate a random number within the range between them
+output the number to the user
+*/
+
+//
+function randNum (lowNum, highNum) {
+  var difference = Math.floor(highNum - lowNum + 1);
+  return Math.floor(Math.random() * difference) + lowNum;
+}
+
+alert('The following prompts will ask for two numbers then generate a random number within that range.')
+
+//collects numbers and converts the string to an int
+var random1 = prompt ("Please enter a number.");
+var random2 = prompt ("Please enter a number.");
+random1 = parseInt(random1);
+random2 = parseInt(random2);
+
+// sorts the numbers into low and high based on their value
+// and creates new variables
+if (random1 > random2) {
+  var low = random2;
+  var high = random1;
+} else {
+  var low = random1;
+  var high = random2;
+}
+
+// provide user with random number by calling the function
+alert('Your random number is ' + randNum(low, high));
