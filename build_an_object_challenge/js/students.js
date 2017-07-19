340020dae2c23101865bebc753030ce7e7efa2fd
skwid138 <skwid138@gmail.com>
2017-07-19 18:50:00 -0500

    added a print function

--- build_an_object_challenge/js/students.js
+++ build_an_object_challenge/js/students.js
@@ -1,3 +1,10 @@
+// prints students info to html div
+function print(message) {
+  var div = document.getElementById('output');
+  div.innerHTML += message;
+}
+
+// student records
 var students = [
   {
     name : 'Hunter',
@@ -30,3 +37,9 @@ var students = [
     points : 86
   }
 ];
+
+//loops through the objects to create html text that prints to the page
+for ( var i = 0; i < students.length; i += 1 ) {
+  var html = "<h2> Student Name: " + students[i].name + '</h2> <br><p>Track: ' + students[i].track + '<br>Achievments: ' + students[i].acheivments + '<br>Points ' + students[i].points + '</p>'  ;
+  print(html);
+}
