5b1ddb39b673f59a8db9f4a61bd3a009886fea6f
skwid138 <skwid138@gmail.com>
2017-07-19 16:42:58 -0500

    created students array with objects inside for the challenge

---
+++ build_an_object_challenge/js/students.js
@@ -0,0 +1,32 @@
+var students = [
+  {
+    name : 'Hunter',
+    track : 'Full Stack Development',
+    acheivments : ['JS Basics', 'HTML Basics', 'CSS Basics'],
+    points : 2892
+  },
+  {
+    name : 'Becky',
+    track : 'SQL',
+    acheivments : ['Smart', 'Beutiful', 'Driven'],
+    points : 5000000000
+  },
+  {
+    name : 'Nebuchadnezzar',
+    track : 'Sleep City',
+    acheivments : ['Big', 'Hairy', 'Audacious'],
+    points : 138
+  },
+  {
+    name : 'Banjo',
+    track : 'Lord of Dogtown',
+    acheivments : ["Wiggle Butt", "Lick Face", "Fast hole digger of 2017"],
+    points : 31
+  },
+  {
+    name : 'Princess Lola',
+    track : 'Barkocolyps',
+    acheivments : ['Dance of Dog', 'Belly Rub Champion', 'SBA Award 2014, 2015, 2016, 2017'],
+    points : 86
+  }
+];
