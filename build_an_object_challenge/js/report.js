6f5e6b827b1f133ac0b7803a2a7856291598a0af
skwid138 <skwid138@gmail.com>
2017-07-26 12:56:58 -0500

    rewrote report builder

--- build_an_object_challenge/js/report.js
+++ build_an_object_challenge/js/report.js
@@ -4,14 +4,26 @@ function print(message) {
   div.innerHTML += message;
 }
 
+function reportBuilder(name) {
+  var report = "<h2> Student Name: " + students[i].name + "</h2>";
+  report += "<p>Track: " + students[i].track + "</p>";
+  report += "<p>Achievments: " + students[i].acheivments + "</p>";
+  report += "<p>Points: " + students[i].points + '</p>';
+  return report;
+}
+
+var html;
+var search;
+
  while (true) {
-  var name = prompt("To search for a student's records, please enter the students name. You can enter as many as you'd like once finished type 'quit' to exit and see the records.").toLowerCase();
-  if ( name === 'quit' || name === null ) {
+  search = prompt("To search for a student's records, please enter the students name. You can enter as many as you'd like once finished type 'quit' to exit and see the records.");
+  search = search.toLowerCase();
+  if ( search.toLowerCase() === 'quit' || search.toLowerCase() === null ) {
     break;
   } else {
       for ( var i = 0; i < students.length; i += 1 ) {
-        if ( name === students[i].name.toLowerCase()) {
-          var html = "<h2> Student Name: " + students[i].name + '</h2> <br><p>Track: ' + students[i].track + '<br>Achievments: ' + students[i].acheivments + '<br>Points ' + students[i].points + '</p>'  ;
+        if ( students[i].name === search ) {
+          html = reportBuilder(search);
           print(html);
         }
       }
