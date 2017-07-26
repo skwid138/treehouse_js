c7281cf7d1fbe08a945e83a34680a9fa383e7666
skwid138 <skwid138@gmail.com>
2017-07-26 13:02:06 -0500

    added comments

--- build_an_object_challenge/js/report.js
+++ build_an_object_challenge/js/report.js
@@ -1,9 +1,13 @@
+var html = '';
+var search;
+
 // prints students info to html div
 function print(message) {
   var div = document.getElementById('output');
   div.innerHTML += message;
 }
 
+// complies student information into html format
 function reportBuilder(name) {
   var report = "<h2> Student Name: " + students[i].name + "</h2>";
   report += "<p>Track: " + students[i].track + "</p>";
@@ -12,9 +16,7 @@ function reportBuilder(name) {
   return report;
 }
 
-var html;
-var search;
-
+// prompts the user for a name to search, then creates a report and prints it to the page
  while (true) {
   search = prompt("To search for a student's records, please enter the students name. You can enter as many as you'd like once finished type 'quit' to exit and see the records.");
   search = search.toLowerCase();
