94986bced0f216e71afab398982cd2f2a265f128
skwid138 <skwid138@gmail.com>
2017-07-26 11:26:41 -0500

    removed semicolons that were not required.

--- build_an_object_challenge/js/report.js
+++ build_an_object_challenge/js/report.js
@@ -14,6 +14,6 @@ function print(message) {
           var html = "<h2> Student Name: " + students[i].name + '</h2> <br><p>Track: ' + students[i].track + '<br>Achievments: ' + students[i].acheivments + '<br>Points ' + students[i].points + '</p>'  ;
           print(html);
         }
-      };
-  };
-};
+      }
+  }
+}
