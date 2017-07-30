4bb637659a7b6d154f35c5b6e6992414341b6e98
skwid138 <skwid138@gmail.com>
2017-07-30 18:20:59 -0500

    created an input field and updated what clicking the button does

--- JavaScript and the DOM/app.js
+++ JavaScript and the DOM/app.js
@@ -1,9 +1,11 @@
 // selects the element in index.html
 // document is the entire page, so this searches the entire webpage for the id
 const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
 
 // take selection and then have browser listen for a click event
-myHeading.addEventListener('click', () => {
+myButton.addEventListener('click', () => {
   // for every click even make the heading red
-  myHeading.style.color = 'red';
-} );
+  myHeading.style.color = myTextInput.value;
+});
